a0eef476bb1ded8cad60103161d11b37eadc9ec1
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-01-30 13:44:30 -0500

    Implement OAuth Routes

--- starting_code/app.js
+++ starting_code/app.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -82,7 +82,9 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/auth/github', passport.authenticate('github', { scope: ['user'] }));
 
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login', successRedirect: '/' }));
 
 
 /*
@@ -95,3 +97,9 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
  * ensureAuthenticated Callback Function
 */
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
